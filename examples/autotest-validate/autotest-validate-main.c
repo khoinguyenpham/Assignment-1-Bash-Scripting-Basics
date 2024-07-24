208c8309d2f151e0e0415b5119947fd5aa568c2d
khoinguyenpham <108651467+khoinguyenpham@users.noreply.github.com>
2024-07-24 17:10:43 +0700

    Update autotest-validate-main.c

--- examples/autotest-validate/autotest-validate-main.c
+++ examples/autotest-validate/autotest-validate-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "autotest-validate.h"
 
-const char * git_username = "akshita-bhasin";
+const char * git_username = "KhoiNguyenPham";
 
 int main(int argc, char**argv)
 {
